82796d6976a8aa14e9293526a7c280b9d009bf0d
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-10 19:02:49 -0600

    created a message function that access the private email property; changed private property to protected property to allow inherited classes to access private property internally

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php echo 'PHP OOP'.'<br/>';
 
-// INHERITANCE
-// when you extend a class, the subclass (children) inherits all methods from the parent
-// unless a class overrides those methods, they will retain their original functionality
-// permits the implementation of additional functionality in similar objects
-// without the need to reimplement all of the shared functionality
+// PROTECTED MODIFIER
+// how can a child class access a private property from the parent class?
+// use 'protected' access modifier rather than private
+// this allows child classes that inherits from the parent class can access that property
+// can only be used internally between classes and does not allow access from outside classes
 
 class User
 {
     public $username;
-    private $email;
+    public $role = 'Member';
+    protected $email;
 
     public function __construct($username, $email)
     {
@@ -20,6 +21,11 @@ class User
     public function addFriend()
     {
         return "{$this->email} added a new friend".'<br/>';
+    }
+
+    public function message()
+    {
+        return "{$this->email} sent a new message";
     }
 
     public function getEmail()
@@ -37,17 +43,19 @@ class User
 
 class AdminUser extends User
 {
-    // to inherit one's class properties/methods, use 'extends'
-    // if adding another prop/method, must use another constructor function
     public $level;
+    public $role = 'Admin';
+    // if wanting to override a property/method then you can set a value in child class
 
     public function __construct($username, $email, $level)
     {
         $this->level = $level;
         parent::__construct($username, $email);
-        // when you have another constructor function in a class that is inheriting
-        // it will override the parent's constructor function
-        // in order to inherit those properties, you can use parent::property/method
+    }
+
+    public function message()
+    {
+        return "{$this->email} sent a new message";
     }
 }
 
@@ -55,10 +63,13 @@ $userOne = new User('Phil', 'phil@philly.com');
 $userTwo = new User('Rod', 'rod@roddy.com');
 $userThree = new AdminUser('Rich', 'rich@richy.com', 8);
 
-echo $userOne->username.'<br/>';
-echo $userThree->username.'<br/>';
+echo $userOne->role.'<br/>';
+echo $userThree->role.'<br/>';
+
+echo $userOne->message().'<br/>';
+echo $userThree->message().'<br/>';
+
 echo $userThree->getEmail().'<br/>';
-echo $userThree->level;
 
 ?>
 
@@ -68,7 +79,7 @@ echo $userThree->level;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP OOP Tutorial</title>
 </head>
 
 <body>
