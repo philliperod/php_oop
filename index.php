c031cbb651a522a8baefa9dd418738082aa61a85
Phillipe Rodriguez <philliperod@gmail.com>
2021-01-12 11:09:30 -0600

    removed previous code and created a form for validation practice; added css styling to form

--- index.php
+++ index.php
@@ -1,44 +1,13 @@
-<?php // STATIC PROPERTIES
-      // declaring class properties or methods as static makes them accessible
-      // without needing an instantiation of the class
-      // a property declared as static cannot be accessed with an instantiated class object
-      // (though a static method can)
-      // because static methods are callable without an instance of the object created
-      // the pseudo-variable '$this' is not available inside the method declared as static
+<?php // VALIDATION class
 
-class Weather
-{
-    public static $tempConditions = ['cold', 'mild', 'warm'];
-
-    public static function celsiusToFarenheit($celsius)
-    {
-        return ($celsius * 9 / 5 + 32).'<br/>';
-    }
-
-    public static function determineTempConditions($farenheit)
-    {
-        if ($farenheit < 40) {
-            return self::$tempConditions[0].'<br/>';
-        }
-        if ($farenheit < 70) {
-            return self::$tempConditions[1].'<br/>';
-        }
-
-        return self::$tempConditions[2].'<br/>';
-        // since an instance does not exist, you use 'self::property'
-        // this will refer to the actual class itself and its property
-        // self = Weather class
-    }
+// Step 1 - check if user's data has been submitted
+if (isset($_POST['submit'])) {
+    //check if the POST request for the submit button has been set
+    // when the button is submit, it will send its value 'submit' to $_POST[value]
+    // then if(isset()) will be true
+    // this is validating entries
+    echo 'Form submitted';
 }
-
-print_r(Weather::$tempConditions);
-// this is a static method
-// without the need to create an instance to reference
-// you can use static properties/methods like so
-// this will have direct access to those properties or methods
-
-echo Weather::celsiusToFarenheit(24);
-echo Weather::determineTempConditions(80);
 
 ?>
 
@@ -49,9 +18,21 @@ echo Weather::determineTempConditions(80);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP Tutorial</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
+    <div class="new-user">
+        <h2>Create new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <!-- when you submit this form it will submit to current file -->
+            <label>Username:</label>
+            <input type="text" name="username">
+            <label>Email:</label>
+            <input type="text" name="email">
+            <input type="submit" value="submit" name="submit">
+        </form>
+    </div>
 
 </body>
 
